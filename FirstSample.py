ea33db589b7c0d1592ba063f46d4124a19e4b4f0
vjaishankarsai <49426491+vjaishankarsai@users.noreply.github.com>
2019-05-06 15:08:06 +0530

    Update FirstSample.py

--- FirstSample.py
+++ FirstSample.py
@@ -1,36 +1,43 @@
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://mongo_container:27017/')
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 mydb = myclient['jai_db']
 mycol = mydb["jai_collection"]
 
 class FirstSample:
-	
-	def add(self,name,address):
-		mdict = { "_id" : name, "address" : address }
-		x=mycol.insert_one(mdict)
-		print("\n *** After Insertion ***\n")
-		self.show()
 
-	def remove(self,name):
-		myquery = { "_id" : name}
-		mycol.delete_one(myquery)
-		print("\n *** After Deletion ***\n")
-		self.show()
+    def add(self, name, address):
+        if mycol.count_documents({'_id': name}) <= 0 :
+            mdict = { "_id": name, "address": address }
+            mycol.insert_one(mdict)
+            print("\n *** After Insertion ***\n")
+            self.show()
+        else:
+            print("Duplicate name found, please give another name")
+
+    def remove(self,name):
+        if mycol.count_documents({'_id': name}) > 0 :
+            myquery = { "_id" : name}
+            mycol.delete_one(myquery)
+            print("\n *** After Deletion ***\n")
+            self.show()
+        else:
+            print("Sorry no such document is present!!!!!!!!!")
 	
-	def modify(self,name,newaddress):
-		myquery = { "_id" : name }
-		newvalues = { "$set" : { "address" : newaddress } }
-		mycol.update_one(myquery,newvalues)
-		print("\n *** After Updation ***\n")
-		self.show()
+    def modify(self,name,newaddress):
+        if mycol.count_documents({'_id': name}) > 0 :
+            myquery = { "_id" : name }
+            newvalues = { "$set" : { "address" : newaddress } }
+            mycol.update_one(myquery,newvalues)
+            print("\n *** After Updation ***\n")
+            self.show()
+        else:
+            print("Unable to Update, the document is not there !!!!!!!!")
 		
-	def show(self):
-		for i in mycol.find():
-			print(i)
-	
-	
-			
+    def show(self):
+        for i in mycol.find():
+            print(i)
+
 flag = True
 fs1 = FirstSample()
 while flag :
@@ -57,5 +64,6 @@ while flag :
 		print("Please select valid option")
 	
 	
+
 
 
